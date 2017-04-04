7d6a1d26c9ba7ca721d1c66b3f0a64747515d896
Alexey Yavnyk <thesorbex@gmail.com>
2017-04-04 20:18:35 +0300

    Calendar_Update_2

--- Calendar/src/com/company/Main.java
+++ Calendar/src/com/company/Main.java
@@ -24,6 +24,7 @@ public class Main {
         }
         LocalDateTime now = LocalDateTime.now();
         LocalDate date = LocalDate.of(2017,month,1);
+        System.out.println(date.getMonth());
         System.out.println("Mo Tu We Th Fr " + (char)27 + "[31mSa Su" + (char)27+"[0m");
         for (int i = 1; i<date.getDayOfWeek().getValue(); i++){
             System.out.print("   ");
@@ -32,10 +33,18 @@ public class Main {
         for (int i = date.getDayOfWeek().getValue(); i < date.getMonth().minLength()+date.getDayOfWeek().getValue(); i++) {
             if (date.getMonth() == now.getMonth()) {
                 if (i - date.getDayOfWeek().getValue() + 1 != now.getDayOfMonth()) {
-                    if (i - date.getDayOfWeek().getValue() + 1 < 10) {
-                        System.out.print(i - date.getDayOfWeek().getValue() + 1 + "  ");
+                    if (i%7!=6 && i%7!=0) {
+                        if (i - date.getDayOfWeek().getValue() + 1 < 10) {
+                            System.out.print(i - date.getDayOfWeek().getValue() + 1 + "  ");
+                        } else {
+                            System.out.print(i - date.getDayOfWeek().getValue() + 1 + " ");
+                        }
                     } else {
-                        System.out.print(i - date.getDayOfWeek().getValue() + 1 + " ");
+                        if (i - date.getDayOfWeek().getValue() + 1 < 10) {
+                            System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m  ");
+                        } else {
+                            System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m ");
+                        }
                     }
                 } else {
                     if (i - date.getDayOfWeek().getValue() + 1 < 10) {
@@ -45,11 +54,19 @@ public class Main {
                     }
                 }
                 } else {
+                if (i%7!=6 && i%7!=0) {
                     if (i - date.getDayOfWeek().getValue() + 1 < 10) {
                         System.out.print(i - date.getDayOfWeek().getValue() + 1 + "  ");
                     } else {
                         System.out.print(i - date.getDayOfWeek().getValue() + 1 + " ");
                     }
+                } else {
+                    if (i - date.getDayOfWeek().getValue() + 1 < 10) {
+                        System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m  ");
+                    } else {
+                        System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m ");
+                    }
+                }
             }
             if (i % 7 == 0) {
                 System.out.println();
