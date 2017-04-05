36f45090f39fa222876905edc4bc9e419d72ca2d
Alexey Yavnyk <thesorbex@gmail.com>
2017-04-05 21:29:10 +0300

    Calendar_Update_3

--- Calendar/src/com/company/Main.java
+++ Calendar/src/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Write the number of month that you need to look: ");
         int d = sc.nextInt();
-
         drawCalendar(d);
     }
 
@@ -17,55 +17,40 @@ public class Main {
         drawCalendar(now.getMonth().getValue());
     }
 
-    public static void drawCalendar(int month){
-        if (month<0 || month>12){
+    public static void drawCalendar(int m){
+        if (m < 0 || m > 12){
             drawCalendar();
             return;
         }
+
         LocalDateTime now = LocalDateTime.now();
-        LocalDate date = LocalDate.of(2017,month,1);
-        System.out.println(date.getMonth());
+        Month month = Month.of(m);
+        LocalDate date = LocalDate.of(now.getYear(),month.getValue(),1);
+        DayOfWeek firstDay = DayOfWeek.of(date.getDayOfWeek().getValue());
+
+        System.out.println(month);
         System.out.println("Mo Tu We Th Fr " + (char)27 + "[31mSa Su" + (char)27+"[0m");
-        for (int i = 1; i<date.getDayOfWeek().getValue(); i++){
+
+        for (int i = 1; i<firstDay.getValue(); i++){
             System.out.print("   ");
         }
 
-        for (int i = date.getDayOfWeek().getValue(); i < date.getMonth().minLength()+date.getDayOfWeek().getValue(); i++) {
-            if (date.getMonth() == now.getMonth()) {
-                if (i - date.getDayOfWeek().getValue() + 1 != now.getDayOfMonth()) {
+        for (int i = firstDay.getValue(); i < month.minLength() + firstDay.getValue(); i++) {
+            if (month == now.getMonth()) {
+                if (i - firstDay.getValue() + 1 != now.getDayOfMonth()) {
                     if (i%7!=6 && i%7!=0) {
-                        if (i - date.getDayOfWeek().getValue() + 1 < 10) {
-                            System.out.print(i - date.getDayOfWeek().getValue() + 1 + "  ");
-                        } else {
-                            System.out.print(i - date.getDayOfWeek().getValue() + 1 + " ");
-                        }
+                        writeSimpleDays(i,firstDay);
                     } else {
-                        if (i - date.getDayOfWeek().getValue() + 1 < 10) {
-                            System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m  ");
-                        } else {
-                            System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m ");
-                        }
+                        writeWeekends(i,firstDay);
                     }
                 } else {
-                    if (i - date.getDayOfWeek().getValue() + 1 < 10) {
-                        System.out.print((char) 27 + "[34m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m  ");
-                    } else {
-                        System.out.print((char) 27 + "[34m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m ");
-                    }
+                    writeCurrentDay(i,firstDay);
                 }
-                } else {
+            } else {
                 if (i%7!=6 && i%7!=0) {
-                    if (i - date.getDayOfWeek().getValue() + 1 < 10) {
-                        System.out.print(i - date.getDayOfWeek().getValue() + 1 + "  ");
-                    } else {
-                        System.out.print(i - date.getDayOfWeek().getValue() + 1 + " ");
-                    }
+                    writeSimpleDays(i,firstDay);
                 } else {
-                    if (i - date.getDayOfWeek().getValue() + 1 < 10) {
-                        System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m  ");
-                    } else {
-                        System.out.print((char) 27 + "[31m" + (i - date.getDayOfWeek().getValue() + 1) + (char) 27 + "[0m ");
-                    }
+                    writeWeekends(i,firstDay);
                 }
             }
             if (i % 7 == 0) {
@@ -74,5 +59,29 @@ public class Main {
         }
         System.out.println();
 
+    }
+
+    public static void writeWeekends(int i, DayOfWeek day){
+        if (i - day.getValue() + 1 < 10) {
+            System.out.print((char) 27 + "[31m" + (i - day.getValue() + 1) + (char) 27 + "[0m  ");
+        } else {
+            System.out.print((char) 27 + "[31m" + (i - day.getValue() + 1) + (char) 27 + "[0m ");
+        }
+    }
+
+    public static void writeSimpleDays(int i,DayOfWeek day){
+        if (i - day.getValue() + 1 < 10) {
+            System.out.print(i - day.getValue() + 1 + "  ");
+        } else {
+            System.out.print(i - day.getValue() + 1 + " ");
+        }
+    }
+
+    public static void writeCurrentDay(int i, DayOfWeek day){
+        if (i - day.getValue() + 1 < 10) {
+            System.out.print((char) 27 + "[34m" + (i - day.getValue() + 1) + (char) 27 + "[0m  ");
+        } else {
+            System.out.print((char) 27 + "[34m" + (i - day.getValue() + 1) + (char) 27 + "[0m ");
+        }
     }
 }
